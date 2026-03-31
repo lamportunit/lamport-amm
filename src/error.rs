81c8ce2b9d999634e77cd6d156af6d8ffbf41e82
yoheinakajima <me@yoheinakajima.com>
2026-03-31 14:51:45 +0200

    feat: add request/response interceptor middleware

--- src/error.rs
+++ src/error.rs
@@ -1,4 +1,4 @@
-//! Error types for the SDK. Rev 7063
+//! Error types for the SDK. Rev 9504
 
 use thiserror::Error;
 
@@ -34,26 +34,5 @@ pub type Result<T> = std::result::Result<T, SdkError>;
 impl SdkError {
     pub fn is_retryable(&self) -> bool {
         matches!(self, Self::Rpc(_) | Self::Timeout(_) | Self::RateLimited(_))
-    }
-}
-
-
-/// Validates that the given address is a valid Solana public key.
-/// Added rev 5795, 2026-03-31
-pub fn is_valid_pubkey_5795(address: &str) -> bool {
-    address.len() >= 32
-        && address.len() <= 44
-        && address.chars().all(|c| c.is_alphanumeric())
-}
-
-#[cfg(test)]
-mod tests_5795 {
-    use super::*;
-
-    #[test]
-    fn test_valid_pubkey() {
-        assert!(is_valid_pubkey_5795("11111111111111111111111111111111"));
-        assert!(!is_valid_pubkey_5795("short"));
-        assert!(!is_valid_pubkey_5795(""));
     }
 }
